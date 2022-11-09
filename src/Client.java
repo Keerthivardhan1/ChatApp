17cdde84c1880526149b867b3c14ae03f6601d13
keerthivardhan1 <keerthivardhantekulapelli@gmail.com>
2022-11-10 01:14:51 +0530

    without_gui

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,98 @@
-package PACKAGE_NAME;public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    Socket socket;
+
+    BufferedReader br;
+    // for output streem  (_---- sends to the client)..
+    PrintWriter out;
+
+
+    public  Client(){
+
+        try{
+            System.out.println("Sending request to the server");
+            socket=new Socket("127.0.0.1",7777);
+            System.out.println("Connection established");
+
+            br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out=new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+
+
+        }catch (Exception e1){
+            e1.printStackTrace();
+
+        }
+    }
+    private void startReading() {
+
+        // this thread read the data
+
+        Runnable r1=()->{
+
+            System.out.println("reader started");
+
+            while (true) {
+                String msg;
+                try {
+                    msg = br.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                if (msg.equals("exit")) {
+                    System.out.println("server terminated the chat");
+                    break;
+                }
+
+                System.out.println(" server :- "+msg);
+            }
+
+        };
+        new Thread(r1).start();
+    }
+
+    private void startWriting() {
+
+        System.out.println("writer is started");
+
+        Runnable r2=()->{
+
+            while (true) {
+
+                try {
+
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                    String content = br1.readLine();
+                    out.println(content);
+                    out.flush();
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                    e.printStackTrace();
+                }
+
+            }
+
+        };
+
+        new Thread(r2).start();
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Client client1=new Client();
+
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,115 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+
+
+public class Server {
+
+    ServerSocket server;
+    Socket socket;
+
+    // for Reading Input streem
+    BufferedReader br;
+    // for output streem  (_---- sends to the client)..
+    PrintWriter out;
+
+
+
+    //Constructor
+    public Server() throws IOException {
+        try {
+            server = new ServerSocket(7777);
+            System.out.println("server is ready to accept the connection");
+            System.out.println("waiting... ");
+            socket=server.accept();
+
+
+            /*getInputStream will return th
+
+            the bit data which is came from the getInputStream() is converted in to charecter
+            and BufferedReader will creat the buffer object
+
+             */
+
+            br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            /*
+
+             */
+
+            out=new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    // We have to read and write simultaneously, so we use the concept of multithreading
+    private void startReading() {
+
+        // this thread read the data
+
+        Runnable r1=()->{
+
+            System.out.println("reader started");
+
+            while (true) {
+                String msg;
+                try {
+                    msg = br.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                if (msg.equals("exit")) {
+                    System.out.println("Clint terminated the chat");
+                    break;
+                }
+
+                System.out.println(" Client :- "+msg);
+            }
+
+        };
+        new Thread(r1).start();
+    }
+
+    private void startWriting() {
+
+        System.out.println("writer is started");
+
+        Runnable r2=()->{
+
+            while (true) {
+
+                try {
+
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                    String content = br1.readLine();
+                    out.println(content);
+                    out.flush();
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                    e.printStackTrace();
+                }
+            }
+
+        };
+
+        new Thread(r2).start();
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Server is created");
+        Server server1=new Server();
     }
 }
