b30807f1a3568324e03d65688ea5a9df291845af
keerthivardhan1 <keerthivardhantekulapelli@gmail.com>
2022-12-21 22:19:11 +0530

    3rd commit

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,32 @@
+package src;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.awt.Font;
 
-public class Client {
+public class Client extends JFrame{
 
     Socket socket;
 
     BufferedReader br;
     // for output streem  (_---- sends to the client)..
     PrintWriter out;
+
+    //components
+
+    private JLabel heading = new JLabel("Client");
+    private JTextArea messageArea = new JTextArea();
+    private JTextField messageInput = new JTextField();
+    private Font font = new Font("Roboto", Font.PLAIN,20);
+
+
 
 
     public  Client(){
@@ -23,16 +39,72 @@ public class Client {
             br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             out=new PrintWriter(socket.getOutputStream());
-
+             createGUI();
+             handelEvents();
             startReading();
-            startWriting();
-
+//            startWriting();
+//
 
         }catch (Exception e1){
             e1.printStackTrace();
 
         }
     }
+
+    private void handelEvents() {
+        messageInput.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+              //  System.out.println("key released is" + e.getKeyCode());
+
+                if(e.getKeyCode() == 10){
+                    String contentToSend = messageInput.getText();
+                    messageArea.append("You : " + contentToSend + "\n");
+                    out.println(contentToSend);
+                    out.flush();
+                    messageInput.setText("");
+                    messageInput.requestFocus();
+                }
+
+            }
+        });
+    }
+
+    private void createGUI() {
+        this.setTitle("Client");
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        // Components
+
+        heading.setFont(font);
+        messageArea.setFont(font);
+        messageInput.setFont(font);
+
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        messageInput.setHorizontalAlignment(SwingConstants.CENTER);
+
+        this.setLayout(new BorderLayout());
+
+        this.add(heading,BorderLayout.NORTH);
+        this.add(messageArea,BorderLayout.CENTER);
+        this.add(messageInput,BorderLayout.SOUTH);
+    }
+
+    //start reading
     private void startReading() {
 
         // this thread read the data
@@ -46,11 +118,14 @@ public class Client {
                   String  msg = br.readLine();
                     if (msg.equals("exit")) {
                         System.out.println("Server terminated the chat");
+                        JOptionPane.showMessageDialog(this , "Server Terminated" );
+                        messageInput.setEnabled(false);
                         socket.close();
                         break;
                     }
 
-                    System.out.println(" server :- " + msg);
+                   // System.out.println(" server :- " + msg);
+                    messageArea.append(" Server : " + msg + "\n");
                 }
 
             } catch (IOException e) {
